7ea253b36a7519aac6575900e6dfe8be9cd50aaa
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-10-06 19:58:46 -0500

    Local Main: repare syntaxerror conecction

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000,
+const port = 3000;
 
-app.get ('/',(request,response) =>{
-  response.send('Send responde express.js')
+app.get('/',(request,response) =>{
+  response.send('Send responde express.js');
 })
 
 app.listen(port, ()=>{
   console.log('Port' + port);
-})
+});
