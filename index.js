d9f471111346b8100192943d9af58a776582fb71
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-10-06 20:19:48 -0500

    Local MAIN: routing express.js

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ app.get('/',(request,response) =>{
   response.send('Send responde express.js');
 })
 
+app.get('/home',(request,response)=>{
+  response.json({
+    "menu":"Inicio",
+    "contactenos":"Contacto"
+  })
+})
+
+app.get('/products',(request,response)=>{
+  response.json({
+    "Polera":150,
+    "Polo":25,
+    "Zapatillas":300
+  })
+})
+
 app.listen(port, ()=>{
   console.log('Port' + port);
 });
