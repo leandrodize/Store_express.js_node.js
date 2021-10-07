c1f3b8e35aeac2fda30b34440f70ad843146de50
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-10-06 19:39:24 -0500

    Local MAIN: Conected server HTTP

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Store");
+const express = require('express');
+const app = express();
+const port = 3000,
+
+app.get ('/',(request,response) =>{
+  response.send('Send responde express.js')
+})
+
+app.listen(port, ()=>{
+  console.log('Port' + port);
+})
