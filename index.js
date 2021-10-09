9ab384d23adc8b1bf0544c1875db859e64e0f9de
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-10-09 01:27:38 -0500

    Local MAIN: limited acces CORS solution

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const routerAPI = require('./Routes');
 const app = express();
+const cors = require('cors');
 const port = 3000;
 const {logErrors, errorHandler,boomError } = require('./Middlewares/indexMiddlewares');
 
 app.use(express.json());
+//Permitir acceso solo a ...
+const whiteList = [
+  'http://localhost:8080',
+  'https://myapp.co'
+];
+const options = {
+  origin: (origin, callback) => {
+    if(whiteList.includes(origin) || !origin){
+      callback(null, true);
+    }else{
+      callback(new Error ('ERROR, NOT PERMITION'))
+    }
+  }
+};
+app,use(cors());
 
 app.get('/',(request,response)=>{
   response.json({
